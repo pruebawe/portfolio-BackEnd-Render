0b61af5314e0bf7da4d02cbcbc38c0e3456e36ab
Miguel Lorente <eldknito.10@gmail.com>
2023-02-16 15:49:29 -0300

    se modifican valores de la entidad experiencia

--- src/main/java/com/portfolio/ML/Entity/Experiencia.java
+++ src/main/java/com/portfolio/ML/Entity/Experiencia.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Experiencia {
@@ -11,6 +13,9 @@ public class Experiencia {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private int id;
+    
+    @NotNull
+    @Size(min = 1, max=50, message="no cumple con la longitud")
     private String nombreE;
     private String fechaIniE;
     private String fechaFinE;
